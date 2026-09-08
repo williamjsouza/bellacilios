b79c04a0ffbba7e1a950883d3cdcd58c95bd599e
ARQ dev Code <arqdevcode@users.noreply.github.com>
2026-09-08 20:20:40 -0300

    chore: default production port to 3020 to prevent server port conflict

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,11 @@
+const PORT = process.env.PORT || 3020;
+
 module.exports = {
   apps: [
     {
       name: 'bellacilios',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 3000',
+      args: `start -p ${PORT}`,
       cwd: './',
       instances: 1, // IMPORTANTE: Manter em 1 para integridade do banco SQLite
       exec_mode: 'fork',
@@ -12,7 +14,7 @@ module.exports = {
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000,
+        PORT: PORT,
       },
     },
   ],
